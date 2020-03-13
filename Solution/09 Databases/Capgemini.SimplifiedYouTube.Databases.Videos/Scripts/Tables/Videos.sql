93f4081092db760f6df015f9f2f148f98e974336
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-03-13 23:09:00 +0800

    init videos database table

---
+++ Solution/09 Databases/Capgemini.SimplifiedYouTube.Databases.Videos/Scripts/Tables/Videos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Videos]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(200) NULL, 
+    [ImageName] NVARCHAR(100) NOT NULL, 
+    [Path] NVARCHAR(200) NOT NULL
+)
